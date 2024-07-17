e03760863a9c1664530dfa5b13f6b5d690f1f051
mohiminulsemon <mohiminulsemon80@gmail.com>
2024-07-17 19:10:30 +0600

    updated

--- online nursery server/src/app/modules/orders/order.route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { OrderControllers } from "./order.controller";
-
-const router = express.Router();
-
-router.post("/create-order", OrderControllers.createOrder);
-router.get("/", OrderControllers.getOrder);
-
-export const  OrderRoutes = router;
