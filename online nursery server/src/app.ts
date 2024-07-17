cb37fbfa92bd2cbeb96aa78db3a742c77e36443f
mohiminulsemon <mohiminulsemon80@gmail.com>
2024-07-17 18:53:35 +0600

    order modules updated

--- online nursery server/src/app.ts
+++ online nursery server/src/app.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import httpStatus from "http-status";
 import { ProductRoutes } from "./app/modules/products/product.route";
+import { OrderRoutes } from "./app/modules/orders/order.route";
 
 const app: Application = express();
 
@@ -11,7 +12,7 @@ app.use(cors());
 
 app.use("/api/products", ProductRoutes);
 // app.use("/api/categories", CategoryRouter);
-// app.use("/api/orders", OrderRouter);
+app.use("/api/orders", OrderRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(httpStatus.OK).json({
